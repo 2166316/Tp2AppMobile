c0590792bcdf642cf74c24a2dd7742275fea048e
2166316 <antou98@live.com>
2023-09-29 17:09:47 -0400

    terminé

---
+++ app/src/main/java/com/example/tp2appmobile/BD/BDHelper.java
@@ -0,0 +1,2 @@
+package com.example.tp2appmobile.BD;public class BD {
+}
---
+++ app/src/main/java/com/example/tp2appmobile/MapPartie2.java
@@ -0,0 +1,14 @@
+package com.example.tp2appmobile;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MapPartie2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_map_partie2);
+    }
+}
